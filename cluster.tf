f8ad45579f4e2af1b6bc568d72356b3bd36521d4
audy <amaudy@gmail.com>
2023-02-11 09:14:09 +0700

    Change cluster spec

--- cluster.tf
+++ cluster.tf
@@ -1,17 +1,17 @@
-resource "scaleway_k8s_cluster" "jack" {
-  name    = "jack"
+resource "scaleway_k8s_cluster" "rose" {
+  name    = "rose"
   version = "1.24.3"
   cni     = "cilium"
 }
 
 resource "scaleway_k8s_pool" "john" {
-  cluster_id = scaleway_k8s_cluster.jack.id
-  name       = "john"
+  cluster_id = scaleway_k8s_cluster.rose.id
+  name       = "jcluster"
   node_type  = "DEV1-M"
-  size       = 3
+  size       = 5
 }
 
 output "kubeconfig" {
   sensitive = true
-  value     = scaleway_k8s_cluster.jack.kubeconfig[0].config_file
+  value     = scaleway_k8s_cluster.rose.kubeconfig[0].config_file
 }
